73a98ab325d5932f364bfa3f9ae0b590af262ac0
Aiko <aikocute@icloud.com>
2022-06-13 13:43:58 +0700

    Update V3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.5.9"
-            versionCode = 205009
+            versionName = "3.0.0"
+            versionCode = 300000
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
