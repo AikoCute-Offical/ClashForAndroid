e637f26a1afd68979d45d191982a1662b248a2b1
AikoCute-Offical <91626055+AikoCute-Offical@users.noreply.github.com>
2022-06-27 14:32:32 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "3.0.1"
-            versionCode = 205009
+            versionName = "1.0.1"
+            versionCode = 100001
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
